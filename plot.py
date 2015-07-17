d2bf27989535159197b00b5f0098d5b41868144d
Adrianna Pińska <adrianna.pinska@gmail.com>
2015-07-17 11:24:30 +0200

    let's split up our problem-checking code into functions

--- plot.py
+++ plot.py
@@ -2,23 +2,30 @@ import numpy
 import matplotlib.pyplot
 import glob
 
-# Now we only calculate the means, maxima and minima once. Not only are we not repeating calculations unnecessarily, but we now use meaningful labels for these values when we use them.
-# How can we do this better?
-# There's a lot of repetition inside these functions. Can we split them up into smaller functions?
+# Let's refactor the problem checking functions first.
 
-def check(maxima, minima):
+#We can make some very simple functions which only check for one problem, and don't print anything.
 
-    # check if the maxima are linear
-    if maxima[0] == 0 and maxima[20] == 20:
-        print "These maxima are suspicious"
+def check_linear(values):
+    "Return True if the values look linear, otherwise return False"
+    return values[0] == 0 and values[20] == 20
+
+def check_zero(values):
+    "Return True if the values add up to zero, otherwise return False"
+    return values.sum() == 0
+
+# Now we can make one function that prints stuff.  This function takes another function as a parameter.
+# We'll be able to call it with the functions we defined above, or any other compatible function we define in the future.
+# In this way we have separated the printing code from the checking code, and we only define each part in one place.
+
+def warn_if_problem_found(values, check_function, name):
+    "Call this check function on these values, and print whether a problem is found"
+    # check_function with no parentheses is the variable name which stores the function.
+    # check_function(...) is how we call the function. This does the same thing as calling check_linear(...) or check_zero(...) directly.
+    if check_function(values):
+        print "These", name, "are suspicious"
     else:
-        print "These maxima look OK"
-
-    # check if the minima are zero
-    if minima.sum() == 0:
-        print "These minima are suspicious"
-    else:
-        print "These minima look OK"
+        print "These", name, "look OK"
 
 def plot(means, maxima, minima):
     fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
@@ -51,5 +58,9 @@ for filename in filenames:
     maxima = data.max(axis=0)
     means = data.mean(axis=0)
     
-    check(maxima, minima)
+    # We have eliminated our old check function entirely, and just call our message-printing function from here directly.
+    # We can mix and match any values with any check function now -- we could check if the minima are linear or if the means are zero.
+    warn_if_problem_found(maxima, check_linear, "maxima")
+    warn_if_problem_found(minima, check_zero, "minima")
+    
     plot(means, maxima, minima)
