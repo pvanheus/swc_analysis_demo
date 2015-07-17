ed2f1b11871c5d4c68fa2ea7c15e3bf8b615e9da
Adrianna Pińska <adrianna.pinska@gmail.com>
2015-07-17 09:37:07 +0200

    find all the files; process just the first three for now

--- plot.py
+++ plot.py
@@ -1,23 +1,30 @@
 import numpy
 import matplotlib.pyplot
+import glob
 
-data = numpy.loadtxt(fname='data/inflammation-01.csv', delimiter=',')
+# use glob.glob to find all the files
+filenames = glob.glob("data/inflammation-*.csv")
+# temporarily pick just the first three
+filenames = filenames[:3]
 
-fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
+for filename in filenames:
+    data = numpy.loadtxt(fname=filename, delimiter=',')
 
-axes1 = fig.add_subplot(1, 3, 1)
-axes2 = fig.add_subplot(1, 3, 2)
-axes3 = fig.add_subplot(1, 3, 3)
+    fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
 
-axes1.set_ylabel('average')
-axes1.plot(data.mean(axis=0))
+    axes1 = fig.add_subplot(1, 3, 1)
+    axes2 = fig.add_subplot(1, 3, 2)
+    axes3 = fig.add_subplot(1, 3, 3)
 
-axes2.set_ylabel('max')
-axes2.plot(data.max(axis=0))
+    axes1.set_ylabel('average')
+    axes1.plot(data.mean(axis=0))
 
-axes3.set_ylabel('min')
-axes3.plot(data.min(axis=0))
+    axes2.set_ylabel('max')
+    axes2.plot(data.max(axis=0))
 
-fig.tight_layout()
+    axes3.set_ylabel('min')
+    axes3.plot(data.min(axis=0))
 
-matplotlib.pyplot.show(fig)
+    fig.tight_layout()
+
+    matplotlib.pyplot.show(fig)
